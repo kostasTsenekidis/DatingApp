399d99c0a35d80e289f4ef14324d62724cc9d964
kostasTsenekidis <tei-kostas@hotmail.com>
2024-05-09 16:23:31 +0300

    End of section 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] 
+public class UserController:ControllerBase
+{
+   private readonly DataContext _context;
+   public UserController(DataContext context)
+   {
+     _context = context;
+   }
+   [HttpGet]
+   public ActionResult<IEnumerable<AppUser>> GetUsers()
+   {
+     var users = _context.Users.ToList();
+
+     return users;
+   }
+
+   [HttpGet("{id}")]  
+     
+   public ActionResult<AppUser> GetUser (int id)
+   {
+      return _context.Users.Find(id);
+   }
+   
+}
+
